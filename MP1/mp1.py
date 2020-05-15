7d603837b0f14a9ad2a509fa5d1963f608180f1d
Kevin <kwhu5695@gmail.com>
2020-05-15 12:39:57 -0500

    adding data

--- MP1/mp1.py
+++ MP1/mp1.py
@@ -1,11 +1,13 @@
 import csv
 import numpy as np
 import math
+import numpy.linalg as linalg
 
 from scipy import signal
 from scipy.signal import butter, lfilter, freqz
 import matplotlib.pyplot as plt
 
+import pandas as pd
 
 # import csv here
 data = []
@@ -14,14 +16,15 @@ data = []
 m_data = []
 
 # replace the string with ("data.csv") before submission
-with open("data.csv") as csvfile:
-    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)  # change contents to floats
-    for row in reader:  # each row is a list
-        data.append(row)
-
-for x in range(len(data)):
-    m_data.append(math.sqrt((data[x][0]) ** 2 + (data[x][1]) ** 2 + (data[x][2]) ** 2))
-
+# with open("../finalProject/data_imu_loc/route1/Accelerometer.csv") as csvfile:
+#     reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)  # change contents to floats
+#     for row in reader:  # each row is a list
+#         data.append(row)
+acc_data = pd.read_csv("../finalProject/data_imu_loc/route1/Accelerometer.csv")
+acc_data = acc_data.to_numpy()
+# for x in range(len(data)):
+#     m_data.append(math.sqrt((data[x][0]) ** 2 + (data[x][1]) ** 2 + (data[x][2]) ** 2))
+m_data = [linalg.norm(i[1:]) for i in acc_data]
 
 # Filter requirements.
 order = 4
@@ -37,7 +40,7 @@ f = open("result.txt", "w+")
 f.write(str(int(steps)))
 f.close()
 plt.subplot(2, 1, 2)
-plt.plot(m_data, 'b-', label='data')
+# plt.plot(m_data, 'b-', label='data')
 plt.plot(f_data, 'g-', linewidth=2, label='filtered data')
 plt.xlabel('Time [sec]')
 plt.grid()
